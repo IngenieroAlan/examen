4f3936328b6e0176c22e560c0e51647bfa7da621
rsuty <mistergamer37@gmail.com>
2024-05-04 12:52:05 -0700

    dom inputs p5js

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,18 @@
-function setup(){
+let timeInput;
+let updateButton;
+
+function setup() {
     createCanvas(windowWidth, windowHeight);
+    timeInput = createInput();
+    timeInput.position(50, 50);
+
+    updateButton = createButton('Actualizar');
+    updateButton.position(timeInput.x + timeInput.width + 10, 50);
 }
-function draw(){
+function draw() {
     background("#d9d9d9");
-    lineaBres(10,10, 50, 70);
-    algoritmoPM(50,100,100);
+    strokeWeight(2);
+    //lineaBres(10, 10, 50, 70);
+    //algoritmoPM(50, 100, 100);
+    text(timeInput.value(), 50, 100);
 }

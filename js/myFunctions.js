be8eedc12c6854ce4a4b3c2410c0c1383250d6ee
rsuty <mistergamer37@gmail.com>
2024-05-06 21:55:35 -0700

    push ver si el brandon checa lo del color y ya

--- js/myFunctions.js
+++ js/myFunctions.js
@@ -1,114 +1,114 @@
 function lineaBres(x1, y1, x2, y2) {
-    let dY = y2 - y1;
-    let dX = x2 - x1;
+  let dY = y2 - y1;
+  let dX = x2 - x1;
 
-    let incYi=0, incXi=0;
-    if (dY >= 0) {
-        incYi = 1;
-    } else {
-        dY = dY*-1;
-        incYi = -1;
-    }
-    if (dX >= 0) {
-        incXi = 1;
-    } else {
-        dX = -1*dX;
-        incXi = -1;
-    }
+  let incYi = 0, incXi = 0;
+  if (dY >= 0) {
+    incYi = 1;
+  } else {
+    dY = dY * -1;
+    incYi = -1;
+  }
+  if (dX >= 0) {
+    incXi = 1;
+  } else {
+    dX = -1 * dX;
+    incXi = -1;
+  }
 
-    let incYr=0 , incXr=0;
-    if (dX >= dY) {
-        incYr = 0;
-        incXr = incXi;
+  let incYr = 0, incXr = 0;
+  if (dX >= dY) {
+    incYr = 0;
+    incXr = incXi;
+  } else {
+    incXr = 0;
+    incYr = incYi;
+    let k = dX;
+    dX = dY;
+    dY = k;
+  }
+  let x = x1;
+  let y = y1;
+  let avR = 2 * dY;
+  let av = avR - dX;
+  let avI = av - dX;
+  while (x != x2 || y != y2) {
+    point(x, y);
+    if (av >= 0) {
+      x = x + incXi;
+      y = y + incYi;
+      av = av + avI;
     } else {
-        incXr = 0;
-        incYr = incYi;
-        let k = dX;
-        dX = dY;
-        dY = k;
+      x = x + incXr;
+      y = y + incYr;
+      av = av + avR;
     }
-    let x = x1;
-    let y = y1;
-    let avR = 2 * dY;
-    let av = avR - dX;
-    let avI = av - dX;
-    while (x != x2 || y != y2) {
-        point(x, y);
-        if (av >= 0) {
-            x = x + incXi;
-            y = y + incYi;
-            av = av + avI;
-        } else {
-            x = x + incXr;
-            y = y + incYr;
-            av = av + avR;
-        }
-    } 
+  }
 }
-function lineaDDA(x1,y1,x2,y2){
+function lineaDDA(x1, y1, x2, y2) {
   let dx = x2 - x1;
-    let dy = y2 - y1;
-    let steps, x_increment, y_increment;
+  let dy = y2 - y1;
+  let steps, x_increment, y_increment;
 
-    if (Math.abs(dx) > Math.abs(dy)) {
-        steps = Math.abs(dx);
-    } else {
-        steps = Math.abs(dy);
-    }
+  if (Math.abs(dx) > Math.abs(dy)) {
+    steps = Math.abs(dx);
+  } else {
+    steps = Math.abs(dy);
+  }
 
-    x_increment = dx / steps;
-    y_increment = dy / steps;
+  x_increment = dx / steps;
+  y_increment = dy / steps;
 
-    let x = x1;
-    let y = y1;
+  let x = x1;
+  let y = y1;
 
-    for (let i = 0; i < steps; i++) {
-        point(Math.round(x), Math.round(y));
-        x += x_increment;
-        y += y_increment;
-    }
+  for (let i = 0; i < steps; i++) {
+    point(Math.round(x), Math.round(y));
+    x += x_increment;
+    y += y_increment;
+  }
 
 }
-function lineaPP(x1,y1,x2,y2){
-  let dx = x2-x1;
-  let dy = y2-y1;
+function lineaPP(x1, y1, x2, y2) {
+  let dx = x2 - x1;
+  let dy = y2 - y1;
   if (dx < 0) {
     [x1, y1, x2, y2] = [x2, y2, x1, y1];
     dx = -dx;
     dy = -dy;
   }
-  let m = dy/dx;
-  let b = y1-m*x1;
-  if(x1!=x2){
+  let m = dy / dx;
+  let b = y1 - m * x1;
+  if (x1 != x2) {
     for (let x = x1; x <= x2; x++) {
       let y = m * x + b;
       point(x, y);
     }
-  }else{
-    let inicio = (y1>y2)?y2:y1;
-    let fin = (y1>y2)?y1:y2;
+  } else {
+    let inicio = (y1 > y2) ? y2 : y1;
+    let fin = (y1 > y2) ? y1 : y2;
     for (let i = inicio; i <= fin; i++) {
       point(x1, i);
     }
   }
 }
-function algoritmoPM(r, xc,yc){
+function algoritmoPM(r, xc, yc) {
   let x = 0, y = r;
-  let pk = 5/4 - r;
+  let pk = 5 / 4 - r;
   point(xc + x, yc + y);
-  trazarSimetria(x, y,xc,yc);
+  trazarSimetria(x, y, xc, yc);
   while (x < y) {
-      x++;
-      if (pk < 0) {
-          pk += 2 * x + 1;
-      } else {
-          y--;
-          pk += 2 * (x - y) + 1;
-      }
-      trazarSimetria(x, y,xc,yc);
+    x++;
+    if (pk < 0) {
+      pk += 2 * x + 1;
+    } else {
+      y--;
+      pk += 2 * (x - y) + 1;
+    }
+    trazarSimetria(x, y, xc, yc);
   }
 }
-function trazarSimetria(x, y,xc,yc) {
+function trazarSimetria(x, y, xc, yc) {
   point(xc + x, yc + y);
   point(xc + y, yc + x);
   point(xc - x, yc + y);
@@ -118,9 +118,9 @@ function trazarSimetria(x, y,xc,yc) {
   point(xc - x, yc - y);
   point(xc - y, yc - x);
 }
-function parseTime(time){
-  if(time<10){
-    return '0'+time;
+function parseTime(time) {
+  if (time < 10) {
+    return '0' + time;
   }
   return time;
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,3 @@
-/*let clockLp, clockCdmx, clockBc;
-let initTime = new Date().toLocaleString('en-US', { timeZone: 'America/Mazatlan' });
-let initTimeValues = {
-    hour: new Date(initTime).getHours(),
-    minutes: new Date(initTime).getMinutes(),
-    seconds: new Date(initTime).getSeconds(),
-};
-let time;
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    console.log('Hora LPZ: '
-    + initTime+'\nHora:'
-    + initTimeValues.hour + '\nMinutos:'
-    + initTimeValues.minutes + '\nSeconds:'
-    + initTimeValues.seconds);
-    let auxTime =parseTime(initTimeValues.hour)+':'+parseTime(initTimeValues.minutes); 
-    clockLp = new Clock(auxTime, "lpz", 150, 250, 250);
-    clockCdmx = new Clock(auxTime, "cdmx", 150, 650, 250);
-    clockBc = new Clock(auxTime, "var", 150, 1050, 250);
-    time = document.getElementById("time");
-}
-function draw() {
-    background("#d9d9d9");
-    clockLp.externalHour = time.value;
-    clockLp.draw();
-    
-    clockCdmx.externalHour = time.value;
-    clockCdmx.draw();
-    
-    clockBc.externalHour = time.value;
-    clockBc.draw();
-}*/
-
-let centerX, centerY;
 let time;
 let hourLength, minuteLength, secondLength;
 let hourWidth, minuteWidth, secondWidth;
@@ -43,7 +9,7 @@ let initTimeValues = {
 };
 let hourH, minutesM, secondsS;
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(windowWidth, 400);
     angleMode(DEGREES);
 
     // Length of the hands
@@ -61,9 +27,14 @@ function setup() {
 function draw() {
     background(220);
     let timerAux = (time.value != undefined && time.value != '') ? time.value : parseTime(initTimeValues.hour) + ':' + parseTime(initTimeValues.minutes);
-    drawClockLpz(timerAux, 100, 100);
-    drawClockCDMX(timerAux, 300, 100);
-    drawClockBC(timerAux, 500, 100);
+    drawClockLpz(timerAux, (windowWidth / 2) - 400, 200);
+    drawClockCDMX(timerAux, windowWidth / 2, 200);
+    drawClockBC(timerAux, (windowWidth / 2) + 400, 200);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("La Paz", (windowWidth / 2) - 400, 200 + 110);
+    text("Ciudad de Mexico", windowWidth / 2, 200 + 110);
+    text("Barcelona", (windowWidth / 2) + 400, 200 + 110);
 }
 
 function drawClockHandDDA(timeValue, timeMax, handLength, handWidth, centerX, centerY) {
@@ -98,18 +69,22 @@ function drawClockLpz(time, centerX, centerY) {
     let hourAngle = map(currentHour % 12, 0, 12, 0, 360) - 90;
     let hourEndX = centerX + hourLength * cos(hourAngle);
     let hourEndY = centerY + hourLength * sin(hourAngle);
+    strokeWeight(4);
     lineaPP(centerX, centerY, parseInt(hourEndX), parseInt(hourEndY));
 
     let minuteAngle = map(currentMinute, 0, 60, 0, 360) - 90;
     let minuteEndX = centerX + minuteLength * cos(minuteAngle);
     let minuteEndY = centerY + minuteLength * sin(minuteAngle);
+    strokeWeight(3);
     lineaPP(centerX, centerY, parseInt(minuteEndX), parseInt(minuteEndY));
 
     let secondAngle = map(new Date().getSeconds(), 0, 60, 0, 360) - 90;
     let secondEndX = centerX + secondLength * cos(secondAngle);
     let secondEndY = centerY + secondLength * sin(secondAngle);
+    color(255, 0, 0);
     lineaPP(centerX, centerY, parseInt(secondEndX), parseInt(secondEndY));
-    algoritmoPM(90, centerX, centerY-10);
+    strokeWeight(4);
+    algoritmoPM(90, centerX, centerY - 10);
 }
 function drawClockCDMX(time, centerX, centerY) {
     let [currentHour, currentMinute] = time.split(":");
@@ -131,7 +106,7 @@ function drawClockCDMX(time, centerX, centerY) {
     let secondEndX = centerX + secondLength * cos(secondAngle);
     let secondEndY = centerY + secondLength * sin(secondAngle);
     lineaDDA(centerX, centerY, parseInt(secondEndX), parseInt(secondEndY));
-    algoritmoPM(90, centerX, centerY-10);
+    algoritmoPM(90, centerX, centerY - 10);
 }
 function drawClockBC(time, centerX, centerY) {
     let [currentHour, currentMinute] = time.split(":");
@@ -142,16 +117,19 @@ function drawClockBC(time, centerX, centerY) {
     let hourAngle = map(currentHour % 12, 0, 12, 0, 360) - 90;
     let hourEndX = centerX + hourLength * cos(hourAngle);
     let hourEndY = centerY + hourLength * sin(hourAngle);
+    strokeWeight(4);
     lineaBres(centerX, centerY, parseInt(hourEndX), parseInt(hourEndY));
 
     let minuteAngle = map(currentMinute, 0, 60, 0, 360) - 90;
     let minuteEndX = centerX + minuteLength * cos(minuteAngle);
     let minuteEndY = centerY + minuteLength * sin(minuteAngle);
+    strokeWeight(3);
     lineaBres(centerX, centerY, parseInt(minuteEndX), parseInt(minuteEndY));
 
     let secondAngle = map(new Date().getSeconds(), 0, 60, 0, 360) - 90;
     let secondEndX = centerX + secondLength * cos(secondAngle);
     let secondEndY = centerY + secondLength * sin(secondAngle);
+    strokeWeight(4);
     lineaBres(centerX, centerY, parseInt(secondEndX), parseInt(secondEndY));
-    algoritmoPM(90, centerX, centerY-10);
+    algoritmoPM(90, centerX, centerY - 10);
 }
